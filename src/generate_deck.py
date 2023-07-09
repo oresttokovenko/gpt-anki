e1c116d97380b73b755ae0732a79752ec67ebe84
Orest Tokovenko <oresttokovenko@gmail.com>
2023-07-09 16:39:36 -0600

    Changed .gitignore to ignore .env file and updated Makefile to include a create-env target for creating a .env file. Deleted java_deck.csv and java.apkg files. Updated requirements.txt to include langchain, pandas, and python-dotenv. Renamed main.py to generate_deck.py and created generate_flashcards.py. Deleted gpt.py file.

--- src/generate_deck.py
+++ src/generate_deck.py
@@ -1,10 +1,14 @@
 import csv
 import genanki
 import sys
+import os
 import random
+from dotenv import load_dotenv
 
-# MODIFY: Replace the following placeholder deck name with your own
-output_deck_name = 'Java'
+load_dotenv("../.env")
+output_deck_name = os.environ.get("DECK_NAME")
+csv_file_path = "../csv/" + output_deck_name + ".csv"
+output_file_path = "../decks/" + output_deck_name + ".apkg"
 
 def read_csv(file_path):
     with open(file_path, 'r', encoding='utf-8') as csv_file:
@@ -41,17 +45,11 @@ def create_anki_deck(deck_name, qa_list):
 def export_anki_deck(deck, output_file):
     genanki.Package(deck).write_to_file(output_file)
 
-def main(input_csv, output_anki, deck_name=output_deck_name):
+def main(input_csv, output_anki, deck_name):
     qa_list = read_csv(input_csv)
     my_deck = create_anki_deck(deck_name, qa_list)
     export_anki_deck(my_deck, output_anki)
-    print(f"Anki deck '{deck_name}' exported to {output_anki}")
+    print(f"Anki deck '{deck_name}' created")
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("Usage: python main.py <input_csv> <output_anki> [deck_name]")
-    else:
-        input_csv = sys.argv[1]
-        output_anki = sys.argv[2]
-        deck_name = sys.argv[3] if len(sys.argv) > 3 else output_deck_name
-        main(input_csv, output_anki, deck_name)
+    main(csv_file_path, output_file_path, output_deck_name)
---
+++ src/generate_flashcards.py
@@ -0,0 +1,69 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts import ChatPromptTemplate
+
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+from typing import List
+import pandas as pd
+import os
+
+# loading api key and defining vars
+
+load_dotenv("../.env")
+deck_name = os.environ.get("DECK_NAME")
+csv_file_path = "../csv/" + deck_name + ".csv"
+
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+model = "gpt-4"
+temperature = 0.0
+
+
+class FlashCard(BaseModel):
+    question: str = Field(description="The question for the flashcard")
+    answer: str = Field(description="The answer for the flashcard")
+
+class FlashCardArray(BaseModel):
+    flashcards: List[FlashCard]
+
+def create_flashcards_from_text(input_text: str, user_prompt: str, csv_file_path: str):
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model=model, temperature=temperature)
+
+    pydantic_parser = PydanticOutputParser(pydantic_object=FlashCardArray)
+
+    format_instructions = pydantic_parser.get_format_instructions()
+
+    prompt = ChatPromptTemplate.from_template(template=user_prompt)
+
+    messages = prompt.format_messages(input_text=input_text, format_instructions=format_instructions)
+
+    output = llm(messages)
+
+    flashcards = pydantic_parser.parse(output.content)
+
+    list_of_flashcards = [card.dict() for card in flashcards.flashcards]
+
+    df = pd.DataFrame(list_of_flashcards)
+
+    if os.path.isfile(csv_file_path):
+        df.to_csv(csv_file_path, mode="a", header=False, index=False)
+    else:
+        df.to_csv(csv_file_path, mode="w", header=False, index=False)
+
+def main():
+    try:
+        with open("input.txt", "r") as f:
+            input_text = f.read()
+
+        with open("prompt.txt", "r") as f:
+            user_prompt = f.read()
+
+        create_flashcards_from_text(input_text, user_prompt, csv_file_path)
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return
+
+
+if __name__ == "__main__":
+    main()
